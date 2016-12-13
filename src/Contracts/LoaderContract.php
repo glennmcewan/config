0bd8bcdd8b9c1f07c8f165511d79a69bfc993e7d
Glenn McEwan <glenn@web-dev.ninja>
2016-12-13 23:30:12 +0000

    Add Loader Contract and Concrete (WIP)

---
+++ src/Contracts/LoaderContract.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Glenn\Config\Contracts;
+
+interface LoaderContract
+{
+    /**
+     * Register a Parser on to the Config Loader.
+     *
+     * @param  ParserContract $parser
+     * @param  string         $key    [optional] Name of the Parser's key, used in @getParser($key)
+     * @author Glenn McEwan <glenn@web-dev.ninja>
+     */
+    public function registerParser(ParserContract $parser, $key = null);
+
+    /**
+     * Get a Parser by its key. Default key scheme is the Parser's FQCN.
+     *
+     * @param  string $key Parser key.
+     * @return ParserContract
+     * @author Glenn McEwan <glenn@web-dev.ninja>
+     */
+    public function getParser($key);
+}
---
+++ src/Loader.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Glenn\Config;
+
+use Glenn\Config\Contracts\LoaderContract;
+use Glenn\Config\Contracts\ParserContract;
+
+class Loader implements LoaderContract
+{
+    /**
+     * The Config Manager
+     * @var ManagerContract
+     */
+    protected $config;
+
+    /**
+     * The available config parsers
+     * @var ParserContract[]
+     */
+    protected $parsers;
+
+    /**
+     * Construct a new Config Loader with the Config to load in to
+     * being passed as a constructor dependency.
+     *
+     * @param  Config $config
+     * @author Glenn McEwan <glenn@web-dev.ninja>
+     */
+    public function __construct(Config $config)
+    {
+        $this->config = $config;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param  ParserContract $parser
+     * @param  string         $key
+     * @return ParserContract
+     * @author Glenn McEwan <glenn@web-dev.ninja>
+     */
+    public function registerParser(ParserContract $parser, $key = null)
+    {
+        if (!$key) {
+            $key = get_class($parser);
+        }
+
+        $this->parsers[$key] = $parser;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param  string         $key Parser Key
+     * @return ParserContract
+     * @author Glenn McEwan <glenn@web-dev.ninja>
+     */
+    public function getParser($key)
+    {
+        if (!array_key_exists($key, $this->parsers)) {
+            return null;
+        }
+
+        return $this->parsers[$key];
+    }
+}
