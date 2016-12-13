5a5041480c770bd0b2a2c505bb4cd84853e308a5
Glenn McEwan <glenn@web-dev.ninja>
2016-12-13 23:24:27 +0000

    Add JSON Parser and Parser Exception

---
+++ src/Parser/JsonParser.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Glenn\Config\Parser;
+
+use Glenn\Config\Contracts\ParserContract;
+
+/**
+ * Parses a JSON string in to a format readable by the Config Manager.
+ *
+ * @author Glenn McEwan <glenn@d3r.com>
+ */
+class JsonParser implements ParserContract
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @param  string $data JSON as a string
+     * @return array        Array of data
+     * @author Glenn McEwan <glenn@web-dev.ninja>
+     */
+    public function parse($data)
+    {
+        $array = json_decode($data, true);
+
+        if ($array === null) {
+            throw new ParseException('Unable to parse JSON.');
+        }
+
+        return $array;
+    }
+}
---
+++ src/Parser/ParseException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Glenn\Config\Parser;
+
+use Exception;
+
+/**
+ * Exception to be used when encountering an issue with parsing data for the config.
+ *
+ * @author Glenn McEwan <glenn@web-dev.ninja>
+ */
+class ParseException extends Exception
+{
+
+}
